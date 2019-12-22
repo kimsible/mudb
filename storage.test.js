cfda3f6cef410a8403999c5451be5305c020c90a
kimsible <kimsible@users.noreply.github.com>
2019-12-22 01:11:32 +0100

    feat(storage): add openSync

--- storage.test.js
+++ storage.test.js
@@ -2,7 +2,7 @@
 
 import test from 'ava'
 import fs from 'fs'
-import { save, open } from './storage'
+import { save, open, openSync } from './storage'
 
 const { access, readFile, writeFile, unlink } = fs.promises
 const name = 'storage.test'
@@ -26,8 +26,22 @@ test('open-existing', async t => {
   t.deepEqual(opened, data)
 })
 
+test('openSync-new', async t => {
+  const opened = openSync(`${name}-openSync-new.json`)
+  await access(`${name}-openSync-new.json`, fs.constants.F_OK)
+  t.deepEqual(opened, [])
+})
+
+test('openSync-existing', async t => {
+  await writeFile(`${name}-openSync-existing.json`, JSON.stringify(data))
+  const opened = openSync(`${name}-openSync-existing.json`)
+  t.deepEqual(opened, data)
+})
+
 test.after.always(async t => {
   await unlink(`${name}-save.json`).catch(() => {})
   await unlink(`${name}-open-new.json`).catch(() => {})
   await unlink(`${name}-open-existing.json`).catch(() => {})
+  await unlink(`${name}-openSync-new.json`).catch(() => {})
+  await unlink(`${name}-openSync-existing.json`).catch(() => {})
 })
